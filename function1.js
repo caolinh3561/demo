f4367135b273e7f26a014e9dad2a407d33061c23
McKylie Tran <baonguyen081296@gmail.com>
2020-08-20 21:14:58 +0700

    bao day file tu nhanh len

--- function1.js
+++ function1.js
@@ -1,7 +1,9 @@
-
+// <<<<<<< HEAD
 console.log("hello mtfcker")
-
+// =======
 console.log("hello");
 // console.log("hello Bao");
 console.log("hello World");
+
+console.log("test đẩy code từ nhánh - Bảo")
 
